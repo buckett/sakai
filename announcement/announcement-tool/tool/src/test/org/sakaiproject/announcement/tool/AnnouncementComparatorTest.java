5a134c50aa530bbc0b09d7ff13d21ef6c88e45e4
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-12-08 09:48:38 +0000

    SAK-30064 Start of unit tests.

--- announcement/announcement-tool/tool/src/test/org/sakaiproject/announcement/tool/AnnouncementComparatorTest.java
+++ announcement/announcement-tool/tool/src/test/org/sakaiproject/announcement/tool/AnnouncementComparatorTest.java
@@ -1,6 +1,37 @@
 package org.sakaiproject.announcement.tool;
-/**
- * Created by buckett on 08/12/2015.
- */
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.sakaiproject.announcement.api.AnnouncementMessage;
+import org.sakaiproject.announcement.api.AnnouncementService;
+import org.sakaiproject.entity.api.EntityPropertyNotDefinedException;
+import org.sakaiproject.entity.api.EntityPropertyTypeException;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.time.api.Time;
+import org.sakaiproject.time.impl.MyTime;
+import org.sakaiproject.util.Resource;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class AnnouncementComparatorTest {
+
+    @Test
+    public void testTimeSort() throws Exception {
+        AnnouncementComparator comparator = new AnnouncementComparator(AnnouncementAction.SORT_DATE, true);
+
+        AnnouncementMessage m1 = Mockito.mock(AnnouncementMessage.class);
+        ResourceProperties p1 = Mockito.mock(ResourceProperties.class);
+        Mockito.when(m1.getProperties()).thenReturn(p1);
+        Time t1 = new MyTime(null, new Date().getTime());
+        Mockito.when(p1.getTimeProperty(AnnouncementService.MOD_DATE)).thenReturn(t1);
+
+        List<AnnouncementMessage> messages = new ArrayList<>();
+
+
+
+
+
+    }
 }
